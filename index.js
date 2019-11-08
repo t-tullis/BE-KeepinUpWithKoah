727940b1e292b037155546e02d06479cf9208e2d
t-tullis <t.tullis34@gmail.com>
2019-11-07 22:19:32 -0800

    changed local port number

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const server = require('./api/server.js');
 const mongoose = require('mongoose')
 
 const connect = () => {
-    server.listen(4500, () => {
-        console.log('Listening to port 4500')
+    server.listen(5000, () => {
+        console.log('Listening to port 5000')
     })
     return mongoose.connect('mongodb://localhost:27017/keepinUpWithKoah', { useNewUrlParser: true })
 }
